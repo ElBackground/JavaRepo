ebf25ce493d67ca5d0b678abce382ec2dab6beff
El Background <elfokbackground@yahoo.com>
2016-11-09 12:29:26 +0300

    Second commit for ServletExplore. 4th servlet changes

--- ServletExplore/src/main/java/com/elbackground/servletexplore/Fourth.java
+++ ServletExplore/src/main/java/com/elbackground/servletexplore/Fourth.java
@@ -3,6 +3,7 @@ package com.elbackground.servletexplore;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collection;
+import java.util.stream.Collectors;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,11 +15,8 @@ public class Fourth extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Collection<String[]> collection = request.getParameterMap().values();
-        String requestBody = "";
-        for (String[] s : collection) {
-            requestBody += "value: " + s[0] + "<br />";
-        }
+        String requestBody = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        
         try (PrintWriter out = response.getWriter()) {
             out.println(
                 Util.defaultWrap("Fourth Servlet", requestBody)
